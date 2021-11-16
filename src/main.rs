992ddfd97f1f400004b6cc3beb678b13333133f3
Yehuthi <93691725+yehuthi@users.noreply.github.com>
2021-11-16 18:28:58 +0200

    Initialize CLI processing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
 fn main() {
-	println!("Hello, world!");
+	let app = clap::App::new(env!("CARGO_PKG_NAME"))
+		.version(env!("CARGO_PKG_VERSION"))
+		.author(env!("CARGO_PKG_AUTHORS"))
+		.about(env!("CARGO_PKG_DESCRIPTION"))
+		.arg(
+			clap::Arg::with_name("URL")
+				.required(true)
+				.help("URL to the image base (temporary, will be URL to the image page)."),
+		);
+	let matches = app.get_matches();
+	let url = matches.value_of("URL").unwrap();
+	dbg!(url);
 }
